3c342f4a015437a92438187f0150b51f5e6c2a01
GabesSeven <vieira.ferreira@unesp.br>
2023-03-04 16:32:47 -0500

    paginacao index completa; inicio criação página para cliente

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import index, register, immobile
+from .views import index, register, immobile, sec_bio
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('admin/', index, name='sec-bio'),
-    path('register:<int:pg>', register, name='register'),
     path('immobile/<int:pk>', immobile, name='immobile'),
+    path('', index, name='index'),
+    path('admin/', sec_bio, name='sec-bio'),
+    path('register:<int:pg>', register, name='register'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,34 +1,21 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.core.paginator import Paginator
 from .forms import ApartamentoModelForm, EdificioModelForm, ClienteModelForm
 from .models import Apartamento, Cliente, Edificio
 
-def immobile(request, pk):
-    # form_cliente = ClienteModelForm(request.POST, request.FILES)
-    print('AQUIIIIIIIII')
-    print(f'PG: {pk}')
-    # if pg != 0 and pg!= 1 and pg != 2:
-        # form_cliente = ClienteModelForm()
-    apartamento = get_object_or_404(Apartamento, id=pk)
+def index(request):
+
+    apartamentos = Apartamento.objects.all()
+    apartamento_paginator = Paginator(apartamentos, 8)
+    page_num = request.GET.get('page')
+    page = apartamento_paginator.get_page(page_num)
+
     context = {
-        'apartamento': apartamento,
+        'page': page,
+        # 'apartamento': apartamentos[1],
     }
-
-    return render(request, 'immobile.html', context)
-
-    # return render(request, 'register.html', context)
-
-
-
-    # if str(request.user) != 'AnonymousUser':
-    # if str(request.method) == 'POST' and pg == 0: # formuláro edificio
-        # validate_form(request, form_cliente)
-        # form_cliente = ClienteModelForm()
-        # context = {
-        # 'form_apartamento': form_apartamento,
-        # 'form_edificio': form_edificio,
-        # 'form_cliente': form_cliente,
-        # }
+    return render(request, 'index.html', context)
 
 def validate_form(request, form):
     if form.is_valid():
@@ -42,7 +29,7 @@ def register(request, pg):
     form_edificio = EdificioModelForm(request.POST, request.FILES)
     form_apartamento = ApartamentoModelForm(request.POST, request.FILES)
 
-    print(f'PG: {pg}')
+    print(f'PK: {pg}')
     if pg != 0 and pg!= 1:
         form_edificio = EdificioModelForm()
         form_apartamento = ApartamentoModelForm()
@@ -67,8 +54,30 @@ def register(request, pg):
         }
     return render(request, 'register.html', context)
 
-def index(request):
+def immobile(request, pk):
+    # form_cliente = ClienteModelForm(request.POST, request.FILES)
+    print('AQUIIIIIIIII')
+    print(pk)
+    # if pg != 0 and pg!= 1 and pg != 2:
+        # form_cliente = ClienteModelForm()
+    # apartamento = get_object_or_404(Apartamento, id=pk)
+    print(dir(request))
+    apartamento = Apartamento.objects.filter(id=pk)
     context = {
-        'apartamentos': Apartamento.objects.all()
+        'apartamento': apartamento,
     }
-    return render(request, 'index.html', context)
+
+    return render(request, 'immobile.html', context)
+
+    # if str(request.user) != 'AnonymousUser':
+    # if str(request.method) == 'POST' and pg == 0: # formuláro edificio
+        # validate_form(request, form_cliente)
+        # form_cliente = ClienteModelForm()
+        # context = {
+        # 'form_apartamento': form_apartamento,
+        # 'form_edificio': form_edificio,
+        # 'form_cliente': form_cliente,
+        # }
+
+def sec_bio():
+    pass
