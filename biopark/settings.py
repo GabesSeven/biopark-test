1fd4b5240cb83909bf157d510a030e71aec0b7f3
Gabriel <37443722+GabesSeven@users.noreply.github.com>
2023-03-06 21:02:04 -0400

    Update settings.py

--- biopark/settings.py
+++ biopark/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'biopark',
         'USER': 'biopark',
-        'PASSWORD': 'senhabiopark',
+        'PASSWORD': 'biopark',
         'HOST': 'localhost',
         'PORT': '3306',
     }
